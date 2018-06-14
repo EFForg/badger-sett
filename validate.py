d7a2f44becfa888d8b40a0f16839bf2d8b9e58da
Bennett Cyphers <bennett@eff.org>
2018-06-13 17:47:53 -0700

    Validate that new maps are not empty
    validate.py errors if the new action_map or snitch_map is empty.

--- validate.py
+++ validate.py
@@ -12,9 +12,17 @@ with open('docker-out/results.json') as f:
 for k in KEYS:
     assert k in new_js
 
+if not len(new_js['snitch_map'].keys()):
+    print("Error: Snitch map empty.")
+    sys.exit(1)
+
+if not len(new_js['action_map'].keys()):
+    print("Error: Action map empty.")
+    sys.exit(1)
 
 old_keys = set(old_js['action_map'].keys())
 new_keys = set(new_js['action_map'].keys())
+
 overlap = old_keys & new_keys
 print("New action map has %d new domains and dropped %d old domains" %
       (len(new_keys - overlap), len(old_keys - overlap)))
