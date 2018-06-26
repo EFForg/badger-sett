f2e1a643c00bdbdc6133e8ef61bfaa3987fc4464
Alexei <alexeiatyahoodotcom@gmail.com>
2018-06-25 21:44:44 -0400

    Summarize what changed in terms of blocked domains
    This compares blocked and cookieblocked base domains.
    
    Blocking different subdomains of the same base domain is not reported.

--- validate.py
+++ validate.py
@@ -3,6 +3,9 @@ import json
 import sys
 import os
 
+import colorama
+import tldextract
+
 # Use: ./validate.py old.json new.json
 KEYS = ['action_map', 'snitch_map', 'version']
 old_path = sys.argv[1]
@@ -40,5 +43,55 @@ new_keys = set(new_js['action_map'].keys())
 overlap = old_keys & new_keys
 print("New action map has %d new domains and dropped %d old domains" %
       (len(new_keys - overlap), len(old_keys - overlap)))
+
+colorama.init()
+C_GREEN = colorama.Style.BRIGHT + colorama.Fore.GREEN
+C_RED = colorama.Style.BRIGHT + colorama.Fore.RED
+C_RESET = colorama.Style.RESET_ALL
+
+extract = tldextract.TLDExtract(cache_file=False)
+
+BLOCKED = ("block", "cookieblock")
+
+blocked_old = {}
+for domain in old_js['action_map'].keys():
+    if old_js['action_map'][domain]['heuristicAction'] not in BLOCKED:
+        continue
+
+    base = extract(domain).registered_domain
+
+    if base not in blocked_old:
+        blocked_old[base] = []
+    blocked_old[base].append(domain)
+
+blocked_new = {}
+for domain in new_js['action_map'].keys():
+    if new_js['action_map'][domain]['heuristicAction'] not in BLOCKED:
+        continue
+
+    base = extract(domain).registered_domain
+
+    if base not in blocked_new:
+        blocked_new[base] = []
+    blocked_new[base].append(domain)
+
+blocked_bases_old = set(blocked_old.keys())
+blocked_bases_new = set(blocked_new.keys())
+
+print("\n{}++{} Newly blocked domains:\n".format(C_GREEN, C_RESET))
+for x in sorted(blocked_bases_new - blocked_bases_old):
+    print("  {}{}{} ({})".format(
+        C_GREEN, x, C_RESET, len(blocked_new[x])))
+    for y in sorted(blocked_new[x]):
+        print("    • {}".format(y))
+
+print("\n{}--{} No longer blocked domains:\n".format(C_RED, C_RESET))
+for x in sorted(blocked_bases_old - blocked_bases_new):
+    print("  {}{}{} ({})".format(
+        C_RED, x, C_RESET, len(blocked_old[x])))
+    for y in sorted(blocked_old[x]):
+        print("    • {}".format(y))
+
+print("")
 
 sys.exit(0)
