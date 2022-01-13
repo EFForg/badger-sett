fa35217b5eed3129402ce80ec682cfab2c67da99
Alexei <alexeiatyahoodotcom@gmail.com>
2022-01-13 13:59:30 -0500

    Revert change to when we log starting a new domain
    The change made exceptional (failure) cases more clear at the expense
    of the common case, where new domains in snitch_map were now
    confusingly logged after starting on the next domain.
    
    This reverts commit f17ccfdde96e0992066e01cf17f0a0f20703e254.

--- crawler.py
+++ crawler.py
@@ -853,8 +853,6 @@ class Crawler:
         random.shuffle(domains)
 
         for i, domain in enumerate(domains):
-            self.logger.info("Visiting %d: %s", i + 1, domain)
-
             try:
                 if self.last_data:
                     old_snitches = self.last_data['snitch_map']
@@ -877,7 +875,8 @@ class Crawler:
                         self.load_user_data(clean_data)
                         self.last_data = clean_data
 
-                # visit the next domain
+                # load the next domain
+                self.logger.info("Visiting %d: %s", i + 1, domain)
                 url = self.get_domain(domain)
                 visited.append(url)
             except ProtocolError as e:
@@ -1064,8 +1063,6 @@ chrome.runtime.sendMessage({
 
         i = None
         for i, domain in enumerate(domains):
-            self.logger.info("Visiting %d: %s", i + 1, domain)
-
             # If we can't load the options page for some reason, treat it like
             # any other error
             try:
@@ -1079,6 +1076,7 @@ chrome.runtime.sendMessage({
                     self.last_data = {}
                     self.restart_browser()
 
+                self.logger.info("Visiting %d: %s", i + 1, domain)
                 url = self.get_domain(domain)
                 visited.append(url)
             except ProtocolError as e:
