a1032df79309d2146236df68309abb626429c096
Alexei <alexeiatyahoodotcom@gmail.com>
2023-10-27 11:41:47 -0400

    Add scan duration column to stats script

--- stats.sh
+++ stats.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-printf "%-14s%-14s%-14s%-14s%-14s%-14s%-14s\n" \
-  BROWSER 'NUM DOMAINS' 'ERROR RATE' 'TIMEOUT RATE' 'NUM RESTARTS' ANTIBOT 'GIT INFO'
+printf "%-12s%-14s%-18s%-14s%-14s%-14s%-14s\n" \
+  BROWSER 'NUM DOMAINS' 'ERRORS (ANTIBOT)' 'TIMEOUT RATE' 'NUM RESTARTS' DURATION 'GIT INFO'
 
 for rev in $(git rev-list HEAD -- log.txt); do
   git --no-pager show "$rev":log.txt > /tmp/log.txt
   num_domains=$(grep 'domains to crawl' /tmp/log.txt | grep -oP '[0-9,]+')
-  printf "%-14s%-14s%-14s%-14s%-14s%-14s%-14s\n" \
+  printf "%-12s%-14s%-18s%-14s%-14s%-14s%-14s\n" \
     "$(grep -oP ''"'"'browserName'"'"': '"'"'[A-Za-z]+'"'"'' /tmp/log.txt | cut -d ' ' -f 2- | sed 's/['"'"']//g')" \
     "$num_domains" \
-    "$(grep 'errored on' /tmp/log.txt | rev | cut -d ' ' -f -2 | rev | cut -d ' ' -f 2- | sed 's/[()]//g')" \
+    "$(grep 'errored on' /tmp/log.txt | rev | cut -d ' ' -f -2 | rev | cut -d ' ' -f 2- | sed 's/[()]//g') ($(($(grep -c 'security page' /tmp/log.txt) * 100 / num_domains))%)" \
     "$(($(grep -c 'Timed out loading ' /tmp/log.txt) * 100 / num_domains))%" \
     "$(grep -c 'restarted' /tmp/log.txt)" \
-    "$(($(grep -c 'security page' /tmp/log.txt) * 100 / num_domains))%" \
+    "$((($(grep 'Finished scan' /tmp/log.txt | cut -d " " -f "1,2" | xargs -0 date +%s -d) - $(head -n1 /tmp/log.txt | cut -d " " -f "1,2" | xargs -0 date +%s -d)) / 3600)) hours" \
     "$(git show -s --format="%h  %ci" "$rev")"
 done
