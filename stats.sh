eaa6f03a70e96ad7003731c5adfa026e377184b5
Alexei <alexeiatyahoodotcom@gmail.com>
2024-02-15 12:16:43 -0500

    Show antibot rate with one decimal place (rounded)

--- stats.sh
+++ stats.sh
@@ -9,7 +9,7 @@ for rev in $(git rev-list HEAD -- log.txt); do
   printf "%-12s%-14s%-18s%-14s%-14s%-14s%-14s\n" \
     "$(grep -oP 'browser: [A-Za-z]+' /tmp/log.txt | cut -d ' ' -f 2- | tr '[:upper:]' '[:lower:]')" \
     "$num_domains" \
-    "$(grep 'errored on' /tmp/log.txt | rev | cut -d ' ' -f -2 | rev | cut -d ' ' -f 2- | sed 's/[()]//g') ($(($(grep -c 'security page' /tmp/log.txt) * 100 / num_domains))%)" \
+    "$(grep 'errored on' /tmp/log.txt | rev | cut -d ' ' -f -2 | rev | cut -d ' ' -f 2- | sed 's/[()]//g') ($(echo "$(grep -c 'security page' /tmp/log.txt) * 100 / $num_domains" | bc -l | xargs printf "%.1f")%)" \
     "$(($(grep -c 'Timed out loading ' /tmp/log.txt) * 100 / num_domains))%" \
     "$(grep -c 'restarted' /tmp/log.txt)" \
     "$((($(grep 'Finished scan' /tmp/log.txt | cut -d " " -f "1,2" | xargs -0 date +%s -d) - $(head -n1 /tmp/log.txt | cut -d " " -f "1,2" | xargs -0 date +%s -d)) / 3600)) hours" \
