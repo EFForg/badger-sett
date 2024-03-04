0233f5113e07dbb187529c185ea500a6d9a2b41f
Alexei <alexeiatyahoodotcom@gmail.com>
2024-03-04 12:04:36 -0500

    Fix running when PSL cache dir doesn't yet exist

--- lib/basedomain.py
+++ lib/basedomain.py
@@ -17,9 +17,14 @@ def extract(domain):
         cache_dir = os.path.join(tempfile.gettempdir(), "python-tldextract")
 
         # expire PSL cache after one week
-        elapsed_time = datetime.now() - datetime.fromtimestamp(os.stat(cache_dir).st_mtime)
-        if elapsed_time >= timedelta(weeks=1):
-            shutil.rmtree(cache_dir)
+        try:
+            mtime = os.stat(cache_dir).st_mtime
+        except FileNotFoundError:
+            pass
+        else:
+            elapsed_time = datetime.now() - datetime.fromtimestamp(mtime)
+            if elapsed_time >= timedelta(weeks=1):
+                shutil.rmtree(cache_dir)
 
         _extract = tldextract.TLDExtract(cache_dir=cache_dir,
                                          include_psl_private_domains=True)
