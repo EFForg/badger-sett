77943f2fdec90555a2a20659c9abd109e1cab9b7
Alexei <alexeiatyahoodotcom@gmail.com>
2024-03-05 16:23:34 -0500

    Add watchdog script for cron

---
+++ misc/watchdog.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")" || exit;
+
+lockdir=../.scan_in_progress
+logfile=../docker-out/log.txt
+
+# if a scan is in progress
+if [ ! -d "$lockdir" ]; then
+  exit
+fi
+
+# and the log file exists
+if [ ! -f "$logfile" ]; then
+  exit
+fi
+
+browser=$(grep -oP 'browser: [A-Za-z]+' "$logfile" | cut -d ' ' -f 2-)
+
+# and if the log file hasn't been updated in a while
+if [ ! "$(find "$logfile" -newermt "6 minutes ago")" ]; then
+  # force a restart by killing the browser
+  if [ "$browser" = Chrome ]; then
+    pkill chrome
+  elif [ "$browser" = Firefox ]; then
+    pkill firefox-bin
+  elif [ "$browser" = Edge ]; then
+    pkill microsoft-edge
+  fi
+fi
