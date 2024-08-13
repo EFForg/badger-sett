fa5dfaa434616942769b1685aebc11794ee4f7ac
Alexei <alexeiatyahoodotcom@gmail.com>
2024-08-13 08:53:51 -0400

    Update tracking domain prevalence summaries
    - Limit to daily non-blocking scans
    - Show absolute as well as relative prevalence

--- initdb.py
+++ initdb.py
@@ -137,36 +137,56 @@ def print_summary(cur):
     cur.execute("SELECT COUNT(*) FROM scan")
     print(f"Rebuilt badger.sqlite3 with data from {cur.fetchone()[0]} scans")
 
+    cur.execute("""
+        SELECT COUNT(DISTINCT tr.site_id)
+        FROM tracking tr
+        JOIN site ON site.id = tr.site_id
+        JOIN scan ON scan.id = tr.scan_id
+        WHERE scan.no_blocking = 1 AND scan.daily_scan = 1
+            AND scan.date > DATETIME('now', '-365 day')""")
+    total_sites = cur.fetchone()[0]
+
     print("\nThe most prevalent (appearing on the greatest number of distinct"
         "\nwebsites) third-party tracking domains over the last 365 days:\n")
     cur.execute("""
-        SELECT t.base, COUNT(distinct tr.site_id) AS num_sites
+        SELECT t.base, COUNT(DISTINCT tr.site_id) AS num_sites
         FROM tracking tr
         JOIN scan ON scan.id = tr.scan_id
         JOIN tracker t ON t.id = tr.tracker_id
-        WHERE scan.date > DATETIME('now', '-365 day')
+        WHERE scan.no_blocking = 1 AND scan.daily_scan = 1
+            AND scan.date > DATETIME('now', '-365 day')
         GROUP BY t.base
         ORDER BY num_sites DESC
         LIMIT 40""")
     top_prevalence = None
+    col_width = None
     for row in cur.fetchall():
         if not top_prevalence:
             top_prevalence = row[1]
-        print(f"  {round(row[1] / top_prevalence, 2):.2f}  {row[0]}")
+            col_width = len(str(top_prevalence))
+        # total site count, site count for tracking domain
+        print(f"  {total_sites}  {row[1]:>{col_width}}  "
+            # absolute prevalence
+            f"{round(row[1] / total_sites, 2):.2f}  "
+            # relative prevalence, tracking domain
+            f"{round(row[1] / top_prevalence, 2):.2f}  {row[0]}")
 
     print("\nThe most prevalent canvas fingerprinters over same date range:\n")
     cur.execute("""
-        SELECT t.base, COUNT(distinct tr.site_id) AS num_sites
+        SELECT t.base, COUNT(DISTINCT tr.site_id) AS num_sites
         FROM tracking tr
         JOIN scan ON scan.id = tr.scan_id
         JOIN tracker t ON t.id = tr.tracker_id
         JOIN tracking_type tt ON tt.id = tr.tracking_type_id
-        WHERE tt.name = 'canvas' AND scan.date > DATETIME('now', '-365 day')
+        WHERE scan.no_blocking = 1 AND scan.daily_scan = 1
+            AND tt.name = 'canvas' AND scan.date > DATETIME('now', '-365 day')
         GROUP BY t.base
         ORDER BY num_sites DESC
         LIMIT 20""")
     for row in cur.fetchall():
-        print(f"  {round(row[1] / top_prevalence, 2):.2f}  {row[0]}")
+        print(f"  {total_sites}  {row[1]:>{col_width}}  "
+            f"{round(row[1] / total_sites, 2):.2f}  "
+            f"{round(row[1] / top_prevalence, 2):.2f}  {row[0]}")
     print()
 
 def ingest_distributed_scans(badger_swarm_dir, cur):
