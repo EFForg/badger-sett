a821d2350e5080ec0ae974aa65d1d55b56185e14
Alexei <alexeiatyahoodotcom@gmail.com>
2024-09-26 12:21:00 -0400

    Tweak MDFP candidates report

--- sql/one-site-only-and-in-mdfp-30d.sh
+++ sql/one-site-only-and-in-mdfp-30d.sh
@@ -1,17 +1,18 @@
 #"/usr/bin/env bash
 
-for line in $(sqlite3 badger.sqlite3 -batch -noheader "SELECT tr.base, fqdn
-  FROM site
-  JOIN tracking t ON t.site_id = site.id
-  JOIN tracker tr ON tr.id = t.tracker_id
-  JOIN scan ON t.scan_id = scan.id
+for line in $(sqlite3 badger.sqlite3 -batch -noheader "SELECT t.base,
+    GROUP_CONCAT(DISTINCT s.fqdn)
+  FROM tracking tr
+  JOIN site s ON s.id = tr.site_id
+  JOIN tracker t ON t.id = tr.tracker_id
+  JOIN scan ON scan.id = tr.scan_id
   WHERE scan.date <= DATETIME('now', '-30 day')
-  GROUP by tr.id
-  HAVING count(*) = 1
-  ORDER BY tr.base ASC"); do
+  GROUP BY t.base
+  HAVING COUNT(*) == 1
+  ORDER BY t.base ASC"); do
 
   tracker="$(echo "$line" | cut -d '|' -f 1)"
-  if grep -q "$tracker" ../privacybadger/src/js/multiDomainFirstParties.js; then
+  if grep -q "\"$tracker\"" ../privacybadger/src/js/multiDomainFirstParties.js; then
     echo "$line";
   fi
 done | column -s '|' -t
