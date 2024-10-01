abc2d866429992de1a5b30866f84ee944d3e81c7
Alexei <alexeiatyahoodotcom@gmail.com>
2024-10-01 12:08:13 -0400

    Add sites with most trackers report

---
+++ sql/sites-top.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sqlite3 badger.sqlite3 -batch "SELECT s.fqdn AS site,
+    COUNT(DISTINCT t.base) AS num_trackers
+  FROM site s
+  JOIN tracking tr ON tr.site_id = s.id
+  JOIN tracker t ON t.id = tr.tracker_id
+  JOIN scan ON scan.id = tr.scan_id
+  WHERE scan.start_time > DATETIME('now', '-30 day')
+  GROUP BY s.id
+  ORDER BY num_trackers DESC
+  LIMIT 30" | column -s '|' -t
