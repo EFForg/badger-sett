94da6a4e07db79de184aa544e726853199c14467
Alexei <alexeiatyahoodotcom@gmail.com>
2024-10-01 21:53:47 -0400

    Tweak scan stats report formatting

--- sql/stats.sh
+++ sql/stats.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sqlite3 badger.sqlite3 -batch "SELECT browser.name AS browser,
+sqlite3 badger.sqlite3 -batch -header -column "SELECT start_time, end_time,
+    ((CAST(STRFTIME('%s', end_time) AS INT) - CAST(STRFTIME('%s', start_time) AS INT)) / 60 / 60) AS num_hours,
+    browser.name AS browser,
     no_blocking,
     num_sites,
-    COUNT(DISTINCT blocked_trackers.tracker_id) AS num_blocked,
-    ((CAST(STRFTIME('%s', end_time) AS INT) - CAST(STRFTIME('%s', start_time) AS INT)) / 60 / 60) AS num_hours,
-    start_time
+    COUNT(DISTINCT blocked_trackers.tracker_id) AS num_blocked
   FROM scan
   JOIN browser ON browser.id = scan.browser_id
   JOIN (SELECT scan.id AS scan_id,
@@ -21,4 +21,4 @@ sqlite3 badger.sqlite3 -batch "SELECT browser.name AS browser,
   WHERE scan.daily_scan = 1
     AND scan.start_time > DATETIME('now', '-30 day')
   GROUP BY scan_id
-  ORDER BY scan.start_time DESC" | column -s '|' -t
+  ORDER BY scan.start_time DESC"
