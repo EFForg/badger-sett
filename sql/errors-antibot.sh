14b85a2174a22e5614b5da18e4caeb26782a14fd
Alexei <alexeiatyahoodotcom@gmail.com>
2026-04-29 11:26:28 -0400

    Tweak scan db reports

--- sql/errors-antibot.sh
+++ sql/errors-antibot.sh
@@ -3,12 +3,11 @@
 sqlite3 badger.sqlite3 -batch -header "SELECT *,
   ROUND(num_antibot * 1.0 / (num_successes + num_antibot) * 100, 1) || '%' AS antibot_rate
     FROM (SELECT STRFTIME('%Y-%m', scan.start_time) AS ym,
-      browser.name AS browser,
       SUM(CASE WHEN site_status.name = 'antibot' THEN 1 ELSE 0 END) AS num_antibot,
       SUM(CASE WHEN site_status.name = 'success' THEN 1 ELSE 0 END) AS num_successes
     FROM scan_sites
     JOIN scan ON scan.id = scan_sites.scan_id
-    JOIN browser ON browser.id = scan.browser_id
     JOIN site_status ON site_status.id = scan_sites.status_id
-    GROUP BY ym, browser.name) AS x
+    WHERE scan.daily_scan = 1
+    GROUP BY ym) AS x
   ORDER BY ym DESC, antibot_rate DESC, num_successes DESC" | column -s '|' -t
--- sql/firefox-only-30d.sh
+++ sql/firefox-only-30d.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 sqlite3 badger.sqlite3 -batch -header "SELECT t.base,
+    COALESCE(GROUP_CONCAT(DISTINCT tt.name), '-') AS 'tracking types',
     COUNT(DISTINCT site.id) num_sites,
     COUNT(DISTINCT s.id) num_scans
   FROM tracker t
   JOIN tracking tr ON tr.tracker_id = t.id
   JOIN scan s ON s.id = tr.scan_id
   JOIN site ON site.id = tr.site_id
+  LEFT JOIN tracking_type tt ON tt.id = tr.tracking_type_id
   WHERE s.browser_id = 1
     AND s.daily_scan = 1
     AND s.start_time > DATETIME('now', '-30 day')
